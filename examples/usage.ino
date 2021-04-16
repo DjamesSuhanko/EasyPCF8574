683d36e8f86420351417ff596cc0af1bfda077cd
David-2D3FE9 <64800039+David-2D3FE9@users.noreply.github.com>
2021-04-16 22:28:03 +0200

    Method added & and minor improvement
    WriteBit method added to allow to write a bit from a parameter source (eg from another function).
    
    startI2C will now push the initial value to PCF8574, otherwise the initial value is useless until the first set/reset/invert.
    
    The example received the new method.
    Because the loop will repeat forever I added a "====>" to the cycle begin in order to make this more visual in serial output
    The serial output display values in BIN format witch is more readable

--- examples/usage.ino
+++ examples/usage.ino
@@ -3,6 +3,12 @@
 EasyPCF8574 pcf_A(0x27,0); //PCF address, initial value
 EasyPCF8574 pcf_B(0x29,0);
 
+/* This below function is for demonstration of "WriteBit" method 
+it is useless in your final code */
+bool MyBoolFunction(){
+    return true;
+}
+
 void setup() {
     Serial.begin(9600);
     //ESP32 example. You can use overloaded function with no parameters in startI2C() method.
@@ -13,43 +19,50 @@ void setup() {
 }
 
 void loop() {
-    Serial.println(" Starting cycle:");
+    Serial.println("==========> New cycle:");
     Serial.println("Initial value (as specified):");
-    Serial.println(pcf_A.getPCFValue());
+    Serial.println(pcf_A.getPCFValue(), BIN);
     delay(500);
     
     Serial.println("Setting value to (39):");
     //00100111
     pcf_A.setFullValue(0x27); //like pcf address :)
     Serial.println("Byte now is:");
-    Serial.println(pcf_A.getPCFValue());
+    Serial.println(pcf_A.getPCFValue(), BIN);
     delay(500);
 
     Serial.println("Checking bit 2 status:"); //bits starts in 0, from right to left
-    Serial.println(pcf_A.getBitValue(2));
+    Serial.println(pcf_A.getBitValue(2), BIN);
     Serial.println("Byte now is:");
-    Serial.println(pcf_A.getPCFValue());
+    Serial.println(pcf_A.getPCFValue(), BIN);
     delay(500);
 
-    Serial.println("Inverting  bit 2 value");
-    pcf_A.setInvertBit(2);
-    Serial.println(pcf_A.getBitValue(2));
+    Serial.println("Inverting  bit 7 value");
+    pcf_A.setInvertBit(7);
+    Serial.println(pcf_A.getBitValue(7), BIN);
     Serial.println("Byte now is:");
-    Serial.println(pcf_A.getPCFValue());
+    Serial.println(pcf_A.getPCFValue(), BIN);
     delay(500);
 
     Serial.println("Set bit 2 up:");
     pcf_A.setUpBit(2);
-    Serial.println(pcf_A.getBitValue(2));
+    Serial.println(pcf_A.getBitValue(2), BIN);
     Serial.println("Byte now is:");
-    Serial.println(pcf_A.getPCFValue());
+    Serial.println(pcf_A.getPCFValue(), BIN);
     delay(500);
 
     Serial.println("Set bit 2 down");
     pcf_A.setDownBit(2);
-    Serial.println(pcf_A.getBitValue(2));
+    Serial.println(pcf_A.getBitValue(2), BIN);
     Serial.println("Byte now is:");
-    Serial.println(pcf_A.getPCFValue());
+    Serial.println(pcf_A.getPCFValue(), BIN);
+    delay(500);
+
+    Serial.println("Copy a bit from parameter");
+    pcf_A.WriteBit(MyBoolFunction(),2);
+    Serial.println(pcf_A.getBitValue(2), BIN);
+    Serial.println("Byte now is:");
+    Serial.println(pcf_A.getPCFValue(), BIN);
     delay(500);
 
     Serial.println("Done.");
